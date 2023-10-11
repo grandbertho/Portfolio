ef6ca976f75ad6383de0b79d519dfca8b3e91332
Michel Bertho <michelbertho25gmail.com>
2023-10-11 12:11:59 -0400

    rewrite

--- script.js
+++ script.js
@@ -24,3 +24,37 @@ link2.addEventListener('click', () => {
 link3.addEventListener('click', () => {
     scrollToElement('.column');
 });
+
+var textParts = ["Computer Engineer", "Freeleancer","Web Developer", "Networker","Cybersecurity", "Sever Manager"];
+var typingSpeed = 120; // Adjust the typing speed (in milliseconds)
+var typingElement = document.getElementById("typing-text");
+var partIndex = 0;
+var letterIndex = 0;
+
+function typeNextPart() {
+  if (partIndex < textParts.length) {
+    var currentPart = textParts[partIndex];
+    var intervalId = setInterval(function() {
+      if (letterIndex < currentPart.length) {
+        typingElement.innerHTML += currentPart.charAt(letterIndex);
+        letterIndex++;
+      } else {
+        clearInterval(intervalId);
+        setTimeout(function() {
+          // Delay before clearing and moving to the next part
+          typingElement.innerHTML = "";
+          partIndex++;
+          letterIndex = 0;
+          typeNextPart(); // Move to the next part
+        }, 1500); // Delay before next part (1 second in this example)
+      }
+    }, typingSpeed);
+  } else {
+    // All parts have been typed, you can restart if needed
+    partIndex = 0;
+    typeNextPart();
+  }
+}
+
+// Start the typing animation
+typeNextPart();
